7f19d7ea16a6003d82b3f99a73322b626cd14d36
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-16 16:46:54 +0100

    Eerste commit

---
+++ src/voertuigen/VoertuigMain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voertuigen;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class VoertuigMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PersonenWagen voertuig = new PersonenWagen("", "BGN 863", 03, -1.1F,1.2F , 4,5);
+        System.out.println(voertuig.toString());
+        voertuig.toon();
+        Voertuig voertuig2 = new Vrachtwagen(0);
+        System.out.println(voertuig.equals(voertuig2));
+        voertuig2.toon();
+        System.out.println(voertuig2.equals(voertuig));
+        System.out.println(voertuig.getKyotoScore());
+        System.out.println(voertuig2.getKyotoScore());
+    }
+    
+}
